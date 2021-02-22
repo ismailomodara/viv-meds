22895393f6758b18d435f99879f91b32e8865801
Ismail Omodara <ismailomodara@Ismails-MacBook-Pro.local>
2021-02-22 20:25:27 +0100

    Completed Covid19 page

---
+++ components/Faq.vue
@@ -0,0 +1,170 @@
+<template>
+  <div>
+    <el-collapse v-model="activeFaq" accordion>
+      <el-collapse-item
+        v-for="(faq, i) in faqs"
+        :key="i"
+        :title="faq.title"
+        :name="i + 1"
+      >
+        <div>
+          <p>{{ faq.content }}</p>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+    <div class="mt-3">
+      <p class="mb-2"><strong>About COVID-19 Vaccine</strong></p>
+      <el-collapse v-model="activeAboutCovid" accordion>
+        <el-collapse-item
+          v-for="(about, i) in aboutCovid"
+          :key="i"
+          :title="about.title"
+          :name="i + 1"
+        >
+          <div>
+            <p>{{ about.content }}</p>
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Faq',
+  data() {
+    return {
+      activeFaq: '',
+      faqs: [
+        {
+          title: 'Will Vivmeds Pharmacy offer the COVID-19 vaccine?',
+          content:
+            'Yes, Vivmeds Pharmacy is part of the federal program to receive the vaccines from the CDC.',
+        },
+        {
+          title: 'When will my pharmacy have the vaccine?',
+          content:
+            'We expect our pharmacy to receive vaccine supply. Currently, supply is allocated by state and local health departments as well as the federal government. Stores may receive supply that is withheld from the general public in order to fulfill second dose appointments or for specific prioritized recipients specified by the state and local health department.',
+        },
+        {
+          title: 'Who will administer the vaccine?',
+          content:
+            'The COVID-19 vaccine will be administered in accordance with state law and federal guidance by our pharmacists, pharmacy interns, and technicians.',
+        },
+        {
+          title:
+            'Do I have to wear a mask to get the vaccine at Vivmeds Pharmacy?',
+          content:
+            "Yes, we require customers to wear face coverings when shopping with us, for their protection and for that of our associates.\nFurthermore, CDC recommends that during the pandemic people age 2 and older should wear masks in public settings and when around people who don't live in their household. This would include visiting a pharmacy or clinic to receive any vaccine, including the COVID-19 vaccine.\n",
+        },
+      ],
+      activeAboutCovid: '',
+      aboutCovid: [
+        {
+          title: 'Will the vaccine be offered to the\n' + 'general public?',
+          content:
+            'Yes. The COVID-19 vaccine should be available or most adults\n' +
+            'this year. Adults that fall into a higher risk category, as designated by the\n' +
+            'CDC and state and local health departments, will have access to the vaccine\n' +
+            'before the general healthy public',
+        },
+        {
+          title: 'Who is immediately eligible for the\n' + 'vaccine?',
+          content:
+            ' Eligibility is determined by state and local health departments based\n' +
+            'on recommendations from the CDC. Please check your state or health department\n' +
+            'website for prioritization criteria and timing',
+        },
+        {
+          title: 'Is the vaccine free?',
+          content:
+            'Yes,\n' +
+            'vaccine doses purchased with U.S. taxpayer dollars will be administered to\n' +
+            'patients with no out-of-pocket cost. Please bring proof of insurance to your\n' +
+            'appointment, including any Medicare, Medicaid, pharmacy, and medical cards. If\n' +
+            'you do not have insurance, please bring state identification/driver’s license\n' +
+            '(if available).',
+        },
+        {
+          title: 'How many doses is the COVID-19\n' + 'vaccine?',
+          content:
+            'Both the Moderna and Pfizer-BioNTech COVID-19 vaccines are 2 doses\n' +
+            'separated by 28 or 21 days respectively. You should not get your second dose\n' +
+            'earlier than recommended, however there is no maximum interval. You should get\n' +
+            'your second dose as close to the recommended interval as possible to be\n' +
+            'protected sooner. A series started with one product must be completed with the\n' +
+            'same product.',
+        },
+        {
+          title:
+            'If I have already had coronavirus, do I still need to be\n' +
+            'vaccinated?',
+          content:
+            'Yes. Reinfection with COVID-19 is uncommon, but it is probable.\n' +
+            'Therefore, the CDC recommends vaccination even if you have had and recovered\n' +
+            'from COVID-19. If you were treated for COVID-19 symptoms with monoclonal\n' +
+            'antibodies or convalescent plasma, you should wait 90 days before getting a\n' +
+            'COVID-19 vaccine.',
+        },
+        {
+          title: 'Are there side effects associated with the vaccine?',
+          content:
+            'The side\n' +
+            'effects from COVID-19 vaccinations may feel like the flu and might affect your\n' +
+            'ability to do daily activities, but they should go away in a few days. Learn\n' +
+            'more about what side effects to expect and get helpful tips on how to reduce\n' +
+            'pain and discomfort after your vaccination. \n CDC is also implementing a new\n' +
+            'smartphone-based tool called v-safe to check in on people’s health after they\n' +
+            'receive a COVID-19 vaccine. When you receive your vaccine, you should also\n' +
+            'receive a v-safe information sheet telling you how to enroll in v-safe. If you\n' +
+            'enroll, you will receive regular text messages directing you to surveys where\n' +
+            'you can report any problems or adverse reactions you have after receiving a\n' +
+            'COVID-19 vaccine.',
+        },
+        {
+          title: 'After I get the vaccine, can I stop wearing a mask?',
+          content:
+            'No. While\n' +
+            'experts learn more about the protection that COVID-19 vaccines provide under\n' +
+            'real-life conditions, it will be important for everyone to continue using all\n' +
+            'the tools available to us to help slow virus spread, like covering your mouth\n' +
+            'and nose with a mask, washing hands often, and staying at least 6 feet away from\n' +
+            'others. Together, the COVID-19 vaccination and following the CDC’s\n' +
+            'recommendations for how to protect yourself and others will offer the best\n' +
+            'protection from getting and spreading COVID-19. Experts need to understand more\n' +
+            'about the protection that COVID-19 vaccines provide before deciding to change\n' +
+            'recommendations on steps everyone should take to slow the spread of the virus\n' +
+            'that causes COVID-19. Other factors, including how many people get vaccinated\n' +
+            'and how the virus is spreading in communities, will also affect this decision.',
+        },
+        {
+          title:
+            'Is it true that the COVID-19 vaccine is only available to people 18+ years of\n' +
+            'age? What should I do to protect my younger children who are not old enough to\n' +
+            'receive the vaccination?',
+          content:
+            'The Pfizer-BioNTech COVID-19 vaccine is presently\n' +
+            'indicated for people 16 years of age and older, while the Moderna vaccine is\n' +
+            'indicated for people 18 years of age and older. While neither COVID-19 vaccine\n' +
+            'is currently recommended for children, ACIP will continue to reevaluate data as\n' +
+            'it becomes available',
+        },
+        {
+          title:
+            'Can I get my COVID-19 vaccine at the same time as other\n' +
+            'vaccines?',
+          content:
+            'Other vaccines should be separated from COVID-19 vaccine by at least\n' +
+            '14 days. If COVID-19 vaccine is accidentally given within 14 days of another\n' +
+            'vaccine, you do not need to restart the COVID-19 vaccine series and should still\n' +
+            'complete it on schedule. CDC may update this recommendation when more data is\n' +
+            'available.',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss"></style>
---
+++ pages/covid-19.vue
@@ -0,0 +1,250 @@
+<template>
+  <div>
+    <PageHeader :title="title" :image="image" />
+    <el-container class="is-flex-column">
+      <div class="vm-section_content">
+        <h5>Free COVID-19 Vaccine</h5>
+        <p>Immunizations will be available at no costs to patients.</p>
+      </div>
+      <div class="vm-section_content">
+        <h5>Signup for COVID-19 Vaccine Information</h5>
+        <el-form ref="form" :model="form" label-position="top">
+          <el-row type="flex" :gutter="40" class="flex-wrap">
+            <el-col :md="12">
+              <el-form-item
+                label="First Name"
+                prop="firstName"
+                :rules="validations.inputField"
+              >
+                <el-input v-model="form.firstName" type="text" />
+              </el-form-item>
+            </el-col>
+            <el-col :md="12">
+              <el-form-item
+                label="Last Name"
+                prop="lastName"
+                :rules="validations.inputField"
+              >
+                <el-input v-model="form.lastName" type="text" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row type="flex" :gutter="40" class="flex-wrap">
+            <el-col :md="24" :lg="10">
+              <el-form-item
+                label="Email"
+                prop="email"
+                :rules="validations.emailField"
+              >
+                <el-input v-model="form.email" type="text" />
+              </el-form-item>
+            </el-col>
+            <el-col :md="24" :lg="14">
+              <el-row type="flex" :gutter="30" class="flex-wrap">
+                <el-col :md="12" :lg="12">
+                  <el-form-item
+                    label="Date of Birth"
+                    prop="dateOfBirth"
+                    :rules="validations.inputField"
+                  >
+                    <el-date-picker v-model="form.dateOfBirth" type="date" />
+                  </el-form-item>
+                </el-col>
+                <el-col :md="12" :lg="12">
+                  <el-form-item
+                    label="Gender"
+                    prop="gender"
+                    :rules="validations.inputField"
+                  >
+                    <el-radio-group v-model="form.gender">
+                      <el-radio-button label="male">Male</el-radio-button>
+                      <el-radio-button label="female">Female</el-radio-button>
+                      <el-radio-button label="others">Others</el-radio-button>
+                    </el-radio-group>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+          <el-row type="flex" :gutter="40" class="flex-wrap">
+            <el-col :span="24">
+              <el-form-item
+                label="Home Address"
+                prop="address"
+                :rules="validations.inputField"
+              >
+                <el-input v-model="form.address" type="text" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row type="flex" :gutter="40" class="flex-wrap">
+            <el-col :span="24">
+              <el-form-item
+                label="Current Medications"
+                prop="currentMedications"
+                :rules="validations.inputField"
+              >
+                <el-input v-model="form.currentMedications" type="text" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row type="flex" :gutter="40" class="flex-wrap">
+            <el-col :md="12">
+              <el-form-item
+                label="Current Pharmacy"
+                prop="currentPharmacy"
+                :rules="validations.inputField"
+              >
+                <el-input v-model="form.currentPharmacy" type="text" />
+              </el-form-item>
+            </el-col>
+            <el-col :md="12">
+              <el-form-item
+                label="Name of Insurance"
+                prop="nameOfInsurance"
+                :rules="validations.inputField"
+              >
+                <el-input v-model="form.nameOfInsurance" type="text" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+        <div class="vm-button__container">
+          <el-button
+            class="el-button--secondary"
+            round
+            :loading="submitting"
+            @click="submit"
+            >Submit</el-button
+          >
+        </div>
+      </div>
+      <div class="vm-section_content">
+        <h5>Steps We've Taken to Keep You Safe</h5>
+        <p>
+          At Vivmeds Pharmacy, we are dedicated to protecting you and your
+          health. Our pharmacy is also taking additional steps to keep you safe:
+        </p>
+        <ul>
+          <li>
+            Screening for COVID-19 symptoms prior to providing immunizations
+          </li>
+          <li>
+            Online Consent & Release Form to streamline vaccine processing and
+            reduce the number of patients in the waiting area
+          </li>
+          <li>
+            Personal protective equipment (PPE), including face shields, for all
+            Vivmeds pharmacy associates.
+          </li>
+          <li>
+            Hand hygiene practiced before and after immunizations, including
+            changing gloves between patients
+          </li>
+          <li>
+            Monitoring COVID transmission and prevalence in the local area to
+            adapt PPE requirements if necessary
+          </li>
+          <li>Keeping social distancing before and after the immunization</li>
+        </ul>
+      </div>
+      <div class="vm-section_content">
+        <Faq />
+      </div>
+      <div class="vm-section_content">
+        <h5>Transfer Your Prescription to Our Pharmacy</h5>
+        <p>
+          Your health is our top priority. That's why we prudently evaluate your
+          medication history when filling your prescriptions. We are always here
+          to answer any questions you may have to provide clarity for an easy
+          experience, as well as peace of mind. .
+        </p>
+        <p>We accept all major insurance plans.</p>
+        <ul>
+          <li>
+            <span>Auto Refill:</span> Enroll at the pharmacy and we'll notify
+            you when your prescriptions are ready for pickup.
+          </li>
+
+          <li>
+            <span>Vaccination Station:</span> Walk-ins welcome for immunizations
+            for flu, shingles, tetanus, and more
+          </li>
+
+          <li>
+            <span>Integrative Medicine and Holistic wellness:</span> Our
+            integrative approach to care at Vivmeds Pharmacy begins with
+            advanced prevention. We want to help you stay healthy, and not fall
+            sick to begin with. We also want to expose any weak links that you
+            might have, so that we can support these weaknesses and prevent
+            problems down the road.
+          </li>
+
+          <li>
+            <span>Non-Sterile Compounding:</span> At Vivmeds Pharmacy, we
+            provide specialty pharmaceutical solutions that typical pharmacies
+            cannot always provide. Our goal is to give you the highest level of
+            quality, customized medications and customer service at peerless
+            prices. Our compounding pharmacy works with patients and physicians
+            to provide a custom solution for their pharmaceutical needs that go
+            beyond what standard pharmacies normally provide.
+          </li>
+
+          <li>
+            <span>Free Delivery Service:</span> We help our patients save time
+            running to the pharmacy and focus on work, hobbies, and other tasks.
+            We conveniently bring to your front door, office, or a more
+            convenient location.
+          </li>
+        </ul>
+      </div>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import validations from '@/mixin/validations'
+import Faq from '@/components/Faq'
+
+export default {
+  name: 'Covid19',
+  components: {
+    Faq,
+  },
+  data() {
+    return {
+      title: 'Covid-19',
+      image: 'covid-19.jpg',
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        dateOfBirth: '',
+        gender: '',
+        address: '',
+        currentMedications: '',
+        currentPharmacy: '',
+        nameOfInsurance: '',
+      },
+      submitting: false,
+      validations,
+    }
+  },
+  methods: {
+    submit() {
+      this.$refs.form.validate((valid) => {
+        if (!valid) {
+          return false
+        }
+        this.submitting = true
+        setTimeout(() => {
+          this.submitting = false
+        }, 2000)
+        return true
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
