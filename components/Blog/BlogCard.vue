2289159ebe6d7f295d8130da14ab9e7e978d6c9e
omodara145 <omodara145@gmail.com>
2021-02-28 09:07:53 +0100

    Started work on Blog Page

---
+++ components/Blog/BlogCard.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="vm-blog-card">
+    <div class="vm-blog-card__image">
+      <img :src="image" alt="" />
+    </div>
+    <div class="vm-blog-card__content">
+      <h3>{{ title }}</h3>
+      <nuxt-link :to="`/${link}`">
+        <el-button type="primary">Read more</el-button>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import image from '@/mixin/image'
+
+export default {
+  name: 'BlogCard',
+  mixins: [image],
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss">
+.vm-blog-card {
+  width: 100%;
+  background: #ffffff;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
+  margin-bottom: 40px;
+
+  &__image {
+    height: 250px;
+    width: 100%;
+
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+
+  &__content {
+    padding: 16px 24px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    height: 140px;
+
+    h3 {
+      color: #292222;
+    }
+
+    p {
+      display: flex;
+      align-items: center;
+      color: #6a8b2c;
+      cursor: pointer;
+
+      span {
+        margin-right: 24px;
+        font-size: 1rem;
+      }
+    }
+  }
+}
+
+@media (max-width: 992px) {
+  h3 {
+    font-size: 1.3rem;
+  }
+}
+
+@media (max-width: 600px) {
+  .vm-blog-card__image {
+    height: 200px;
+  }
+
+  .vm-blog-card__content {
+    height: 120px;
+  }
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <PageHeader :title="title" :image="image" />
+    <el-container class="vm-section">
+      <el-row type="flex" :gutter="30" class="flex-wrap">
+        <el-col v-for="(blog, i) in blogs" :key="i" :sm="12" :md="12" :lg="8">
+          <BlogCard :image="blog.image" :title="blog.title" :link="blog.link" />
+        </el-col>
+      </el-row>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import BlogCard from '@/components/Blog/BlogCard'
+
+export default {
+  name: 'Blog',
+  components: {
+    BlogCard,
+  },
+  data() {
+    return {
+      title: 'Blog',
+      image: 'blog.jpg',
+      blogs: [
+        {
+          title: 'Test',
+          image:
+            'https://www.pexels.com/photo/bunch-of-white-oval-medication-tablets-and-white-medication-capsules-159211/',
+          slug: 'test',
+        },
+        {
+          title: 'Test2',
+          image:
+            'https://www.pexels.com/photo/bunch-of-white-oval-medication-tablets-and-white-medication-capsules-159211/',
+          slug: 'test',
+        },
+        {
+          title: 'Test3',
+          image:
+            'https://www.pexels.com/photo/bunch-of-white-oval-medication-tablets-and-white-medication-capsules-159211/',
+          slug: 'test',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
