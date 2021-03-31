39037349f87b8af4d712c9fce740464337dc03d0
omodara145 <omodara145@gmail.com>
2021-03-31 17:12:40 +0100

    Implemented SEO and unfurl image

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,82 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'VivMeds',
+    title: 'VivMeds Pharmacy',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+
+      { name: 'author', content: 'VivMeds Pharmacy' },
+      { name: 'theme-color', content: '#6A8B2C' },
+      {
+        name: 'description',
+        content:
+          'At Vivmeds Pharmacy , we work directly with you to determine where there may be medication-related problems or concerns. Medication management will ensure that all of your medications are consistent with each other and with you.',
+      },
+      {
+        name: 'keywords',
+        content:
+          'VivMeds Pharmacy, Vivmeds, vivmeds, viv-meds, pharmacy, compounding services, covid-19',
+      },
+
+      // Schema.org markup for Google+
+      { itemprop: 'name', content: 'VivMeds' },
+      { itemprop: 'description', content: 'VivMeds Pharmacy' },
+      {
+        itemprop: 'image',
+        content: 'https://vivmeds.com/vivmedscard.png',
+      },
+
+      // Twitter Card data
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@vivmeds' },
+      { name: 'twitter:title', content: 'VivMeds Pharmacy' },
+      { name: 'twitter:url', content: 'https://vivmeds.com' },
+      {
+        name: 'twitter:image',
+        content: 'https://vivmeds.com/vivmedscard.png',
+      },
+      {
+        name: 'twitter:description',
+        content:
+          'At Vivmeds Pharmacy , we work directly with you to determine where there may be medication-related problems or concerns. Medication management will ensure that all of your medications are consistent with each other and with you.',
+      },
+      { name: 'twitter:app:country', content: 'NG' },
+      { name: 'twitter:creator', content: '@vivmeds' },
+      { name: 'twitter:domain', content: '@vivmeds' },
+      {
+        name: 'twitter:image:src',
+        content: 'https://vivmeds.com/vivmedscard.png',
+      },
+
+      // Open Graph data
+      { property: 'og:title', content: 'VivMeds Pharmacy' },
+      { property: 'og:url', content: 'https://vivmeds.com' },
+      {
+        property: 'og:image',
+        content: 'https://vivmeds.com/vivmedscard.png',
+      },
+      {
+        property: 'og:description',
+        content:
+          'At Vivmeds Pharmacy , we work directly with you to determine where there may be medication-related problems or concerns. Medication management will ensure that all of your medications are consistent with each other and with you',
+      },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:site_name', content: 'VivMeds Pharmacy' },
+      {
+        hid: 'og:type',
+        property: 'og:site_name',
+        content: 'VivMeds Pharmacy',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'VivMeds Pharmacy',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
